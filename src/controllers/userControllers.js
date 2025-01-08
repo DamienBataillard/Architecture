051cad3709b1af660fdffdafc833f415ccd478ea
DamienBataillard <damien.bataillard@efrei.net>
2025-01-08 10:04:31 +0100

    userControllers.js

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,32 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+// Inscription d'un utilisateur
+exports.registerUser = async (req, res) => {
+    const { name, email, password, role } = req.body;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ name, email, password: hashedPassword, role });
+        res.status(201).json({ message: 'User created', user });
+    } catch (error) {
+        res.status(400).json({ message: 'Error creating user', error });
+    }
+};
+
+// Connexion d'un utilisateur
+exports.loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ where: { email } });
+        if (!user) return res.status(404).json({ message: 'User not found' });
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) return res.status(401).json({ message: 'Invalid credentials' });
+
+        res.status(200).json({ message: 'Login successful', user });
+    } catch (error) {
+        res.status(500).json({ message: 'Error logging in', error });
+    }
+};
