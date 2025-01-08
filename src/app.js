5656efca18a727e3d3dc552fb1ee33f2afcded5c
DamienBataillard <damien.bataillard@efrei.net>
2025-01-08 10:01:29 +0100

    app.js update

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/database');
+const User = require('./models/User');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +15,10 @@ app.use(express.json());
 sequelize.authenticate()
     .then(() => console.log('Database connected'))
     .catch(err => console.error('Database connection failed:', err));
+
+sequelize.sync({ force: false }) // `force: true` recrée les tables à chaque démarrage
+    .then(() => console.log('Database synced'))
+    .catch(err => console.error('Sync error:', err));
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
