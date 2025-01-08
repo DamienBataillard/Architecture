9a08fce829dd97fb2e00db88ff3c01094fed2ef1
DamienBataillard <damien.bataillard@efrei.net>
2025-01-08 11:13:14 +0100

    userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -14,10 +14,10 @@ router.get('/', async (req, res) => {
 
 // Ajouter un nouvel utilisateur
 router.post('/', async (req, res) => {
-    const { name, email, password } = req.body;
+    const { name, email, password, role } = req.body;
 
     try {
-        const user = await User.create({ name, email, password });
+        const user = await User.create({ name, email, password, role });
         res.status(201).json({ message: 'User created successfully', user });
     } catch (error) {
         res.status(500).json({ message: 'Error creating user', error });
